b3152a0b4f799fb997c5a4ce9ba0b87886995727
Md-Fazil <fuzyll10598@gmail.com>
2021-05-25 11:17:45 +0800

    Add server side paging

--- Data/Services/PublishersService.cs
+++ Data/Services/PublishersService.cs
@@ -1,4 +1,5 @@
 ﻿using my_books.Data.Models;
+using my_books.Data.Paging;
 using my_books.Data.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -16,9 +17,10 @@ namespace my_books.Data.Services
             _context = context;
         }
 
-        public List<Publisher> GetAllPublishers(string sortBy, string searchString)
+        public List<Publisher> GetAllPublishers(string sortBy, string searchString, int? pageNumber)
         {
             var allPublishers = _context.Publishers.OrderBy(n => n.Name).ToList();
+
             if (!string.IsNullOrEmpty(sortBy))
             {
                 switch (sortBy) 
@@ -35,6 +37,11 @@ namespace my_books.Data.Services
             {
                 allPublishers = allPublishers.Where(n => n.Name.Contains(searchString, StringComparison.CurrentCultureIgnoreCase)).ToList();
             }
+
+            //Paging
+            int pageSize = 5;
+            allPublishers = PaginatedList<Publisher>.Create(allPublishers.AsQueryable(), pageNumber ?? 1, pageSize);
+
             return allPublishers;
         }
 
